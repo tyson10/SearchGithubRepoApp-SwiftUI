d419f6d3a77d7ef6aad7866ba131d0fdda3b3711
Taeyoung Son <taeyoungson10@gmail.com>
2023-01-27 23:49:09 +0900

    build: 기존 파일 제거, tuist init

--- README.md
+++
@@ -1,2 +0,0 @@
-# SearchGithubRepoApp-SwiftUI
-SwiftUI를 학습하기 위한 Github 저장소 검색 예제 앱
---
+++ Targets/SearchGithubRepoAppSwiftUIUI/Tests/SearchGithubRepoAppSwiftUIUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class SearchGithubRepoAppSwiftUIUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("SearchGithubRepoAppSwiftUIUI", "SearchGithubRepoAppSwiftUIUI")
+    }
+}
