7530484e1eaef6380dcdc40e37ebb3274ae753c4
Taeyoung Son <taeyoungson10@gmail.com>
2023-01-27 23:41:17 +0900

    feat: 프로젝트 생성

---
+++ SearchGithubRepoApp/SearchGithubRepoApp/SearchGithubRepoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchGithubRepoAppApp.swift
+//  SearchGithubRepoApp
+//
+//  Created by Taeyoung Son on 2023/01/27.
+//
+
+import SwiftUI
+
+@main
+struct SearchGithubRepoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
