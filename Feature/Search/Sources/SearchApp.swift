51ab6a86b12b9be535001286e6cf66618d168889
Taeyoung Son <taeyoungson10@gmail.com>
2023-01-30 23:32:06 +0900

    feat: App Entrypoint 추가 및 ContentView 추가

---
+++ Feature/Search/Sources/SearchApp.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  Search
+//
+//  Created by Taeyoung Son on 2023/01/30.
+//
+
+import Foundation
+
+import SwiftUI
+
+@main
+struct ToyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
