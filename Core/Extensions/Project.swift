c6ea8ec5efdf3b9a221f1ce28b17074f53a4d331
Taeyoung Son <taeyoungson10@gmail.com>
2023-02-01 18:11:26 +0900

    build: Extensions 프로젝트 생성

---
+++ Core/Extensions/Project.swift
@@ -0,0 +1,11 @@
+//
+//  Project.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by Taeyoung Son on 2023/02/01.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.feature(name: "Extensions", products: [.dynamicLibrary], dependencies: [])
