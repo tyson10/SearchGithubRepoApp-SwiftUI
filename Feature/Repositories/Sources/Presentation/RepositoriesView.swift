15a2f6882b9d4a0e01419f3c91d2a97d0e757aff
Taeyoung Son <taeyoungson10@gmail.com>
2023-02-04 14:24:08 +0900

    feat: RepositoriesView 생성

---
+++ Feature/Repositories/Sources/Presentation/RepositoriesView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct RepositoriesView: View {
+    var body: some View {
+        List {
+            
+        }
+    }
+}
+
+struct RepositoriesView_Previews: PreviewProvider {
+    static var previews: some View {
+        RepositoriesView()
+    }
+}
