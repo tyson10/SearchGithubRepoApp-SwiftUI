706d2ee9fec7fd61037ec25d1a0261f176c2607c
Taeyoung Son <taeyoungson10@gmail.com>
2023-02-04 16:19:15 +0900

    feat: ImageLabel 생성(구현 필요)

---
+++ Common/UI/Sources/ImageLabel.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+public struct ImageLabel: View {
+    public var body: some View {
+        Spacer()
+    }
+}
+
+struct RepositoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageLabel()
+    }
+}
