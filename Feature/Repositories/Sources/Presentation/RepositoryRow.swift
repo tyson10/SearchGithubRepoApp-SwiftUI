3c8fa434eaef239ed7f99e6cd20a9bd3ee03cd6e
Taeyoung Son <taeyoungson10@gmail.com>
2023-02-04 15:01:49 +0900

    feat: Row에 레이아웃 추가(추후 적절한 컴포넌트로 대체)

--- Feature/Repositories/Sources/Presentation/RepositoryRow.swift
+++ Feature/Repositories/Sources/Presentation/RepositoryRow.swift
@@ -9,8 +9,32 @@ import SwiftUI
 
 struct RepositoryRow: View {
     var body: some View {
-        List {
+        VStack(alignment: .leading) {
+            HStack {
+                Color(.black)
+                    .frame(width: 20, height: 20)
+                
+                Color(.black)
+                    .frame(maxHeight: 20)
+            }
             
+            Text("tmap")
+            
+            Text("R package for maps")
+            
+            HStack {
+                Color(.black)
+                    .frame(width: 20, height: 20)
+                
+                Text("659")
+                    .frame(maxHeight: 20)
+                
+                Color(.black)
+                    .frame(width: 20, height: 20)
+                
+                Text("R")
+                    .frame(maxHeight: 20)
+            }
         }
     }
 }
