fd3fc1d609e3abdf2f8eec1e8390a1dc9073bda6
Taeyoung Son <taeyoungson10@gmail.com>
2023-02-04 13:31:28 +0900

    feat: 입력값에 따라서 matchedQueries 에 반영 및 UI에 반영

--- Feature/Search/Sources/Presentaion/SearchView.swift
+++ Feature/Search/Sources/Presentaion/SearchView.swift
@@ -12,6 +12,7 @@ import Extensions
 struct SearchView: View {
     @AppStorage("RecentlyQueries") private var recentlyQueries: [String] = []
     @State private var searchQueryStr: String = ""
+    @State private var matchedQueries: [String] = []
     
     var body: some View {
         NavigationView {
@@ -21,7 +22,7 @@ struct SearchView: View {
                     // 기본 상태에선 준수하지 못함.
                     // 'id: \.self' 이 Element들의 해시값으로 구분하도록 함.
                     // 고로, Element는 Hashable이어야 한다.
-                    ForEach(self.$recentlyQueries, id: \.self) {
+                    ForEach($matchedQueries, id: \.self) {
                         RecentSearchesContentView(value: $0.wrappedValue)
                     }
                 } header: {
@@ -31,8 +32,11 @@ struct SearchView: View {
             }
             .navigationTitle("Github")
         }
-        .searchable(text: self.$searchQueryStr,
+        .searchable(text: $searchQueryStr,
                     prompt: "Search Repositories")
+        .onChange(of: searchQueryStr) { newValue in
+            matchedQueries = recentlyQueries.filter { $0.hasPrefix(newValue) }
+        }
     }
 }
 
