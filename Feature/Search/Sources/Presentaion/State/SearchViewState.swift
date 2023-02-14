d98dc4d8453aee37e1e3a121a4827ba3e1edaadf
Taeyoung Son <taeyoungson10@gmail.com>
2023-02-15 01:03:18 +0900

    feat: State 객체에 API 요청하는 동작 추가(테스트 필요)

--- Feature/Search/Sources/Presentaion/State/SearchViewState.swift
+++ Feature/Search/Sources/Presentaion/State/SearchViewState.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import Combine
 
 import Network
+import Model
 
 class SearchViewState: ObservableObject {
     @AppStorage("RecentlyQueries") private var recentlyQueries: [String] = []
@@ -17,7 +18,23 @@ class SearchViewState: ObservableObject {
     
     private let networkService: NetworkService
     
+    var subscriptions = Set<AnyCancellable>()
+    
     init(networkService: NetworkService = NetworkService()) {
         self.networkService = networkService
+    }
+    
+    func search(name: String) {
+        self.networkService.request(endPoint: .search(option: .init(name: name)))
+            .decode(type: Repositories.self, decoder: JSONDecoder())
+            .sink { completion in
+                print("completion: \(completion)")
+            } receiveValue: { repos in
+                print("repos: \(repos)")
+            }
+            .store(in: &subscriptions)
+        
+        // TODO: subscriptions에 누적되어 메모리 누수 있을것으로 예상되므로 확인 필요
+//        print("count: \(self.subscriptions.count)")
     }
 }
