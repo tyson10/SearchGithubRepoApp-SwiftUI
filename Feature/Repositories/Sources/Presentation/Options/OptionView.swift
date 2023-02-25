631e4c8d4ec753350595bbd13aa8752d09c67616
Taeyoung Son <taeyoungson10@gmail.com>
2023-02-25 22:54:14 +0900

    feat: OptionView 생성 - 파일 정리

---
+++ Feature/Repositories/Sources/Presentation/Options/OptionView.swift
@@ -0,0 +1,18 @@
+//
+//  OptionView.swift
+//  Repositories
+//
+//  Created by Taeyoung Son on 2023/02/25.
+//
+
+import SwiftUI
+
+import Model
+
+public struct OptionView: View {
+    public var body: some View {
+        List {
+            
+        }
+    }
+}
