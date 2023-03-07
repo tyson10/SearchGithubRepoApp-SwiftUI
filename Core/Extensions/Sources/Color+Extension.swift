5da761046dfce32d10b293bd6b35af318277a191
Taeyoung Son <taeyoungson10@gmail.com>
2023-03-07 21:53:26 +0900

    feat: Color+Extension 추가

---
+++ Core/Extensions/Sources/Color+Extension.swift
@@ -0,0 +1,28 @@
+//
+//  Color+Extension.swift
+//  Extensions
+//
+//  Created by Taeyoung Son on 2023/03/07.
+//
+
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        let r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (r, g, b) = ((int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (r, g, b) = (int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (r, g, b) = (int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (r, g, b) = (0, 0, 0)
+        }
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255)
+    }
+}
