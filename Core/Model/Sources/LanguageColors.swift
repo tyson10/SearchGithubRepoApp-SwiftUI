51ca4fd0d68518e9ccea8dda5281b3e962871f21
Taeyoung Son <taeyoungson10@gmail.com>
2023-03-07 22:34:32 +0900

    feat: LanguageColors 모델들 추가

---
+++ Core/Model/Sources/LanguageColors.swift
@@ -0,0 +1,41 @@
+//
+//  LanguageColors.swift
+//  Model
+//
+//  Created by Taeyoung Son on 2023/03/07.
+//
+
+import SwiftUI
+
+import Extensions
+
+public typealias LanguageColors = [String: LanguageColorValue]
+
+public struct LanguageColorValue: Codable {
+    let hexColor: String?
+    let url: String
+    
+    var color: Color? {
+        guard let hex = self.hexColor else { return nil }
+        return .init(hex: hex)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case hexColor = "color"
+        case url
+    }
+}
+
+public class LangColorPalette {
+    public static var shared = LangColorPalette()
+    public var colors = LanguageColors()
+    
+    public func set(colors: LanguageColors) {
+        self.colors = colors
+    }
+    
+    public func color(language: String?) -> Color? {
+        guard let lang = language else { return nil }
+        return self.colors[lang]?.color
+    }
+}
