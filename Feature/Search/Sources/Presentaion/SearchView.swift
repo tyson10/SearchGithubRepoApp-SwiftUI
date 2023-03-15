93ce3418dc09c8fc670232ddf1f9ac8c15213efc
Taeyoung Son <taeyoungson10@gmail.com>
2023-03-16 00:02:30 +0900

    feat: 검색키 입력 동작 추가

--- Feature/Search/Sources/Presentaion/SearchView.swift
+++ Feature/Search/Sources/Presentaion/SearchView.swift
@@ -14,29 +14,39 @@ struct SearchView: View {
     @AppStorage("RecentlyQueries") private var recentlyQueries: [String] = []
     @State private var searchQueryStr: String = ""
     @State private var matchedQueries: [String] = []
+    @State private var pushActive = false
     
     var body: some View {
-        NavigationView {
-            List {
-                Section {
-                    // self.recentlyQueries의 Element는 Identifiable을 준수해야 함.
-                    // 기본 상태에선 준수하지 못함.
-                    // 'id: \.self' 이 Element들의 해시값으로 구분하도록 함.
-                    // 고로, Element는 Hashable이어야 한다.
-                    ForEach($matchedQueries, id: \.self) {
-                        RecentSearchesContentView(value: $0.wrappedValue)
+        NavigationStack {
+            NavigationView {
+                List {
+                    Section {
+                        // self.recentlyQueries의 Element는 Identifiable을 준수해야 함.
+                        // 기본 상태에선 준수하지 못함.
+                        // 'id: \.self' 이 Element들의 해시값으로 구분하도록 함.
+                        // 고로, Element는 Hashable이어야 한다.
+                        ForEach($matchedQueries, id: \.self) {
+                            RecentSearchesContentView(value: $0.wrappedValue)
+                        }
+                    } header: {
+                        RecentSearchesHeaderView()
+                            .textCase(.none)
                     }
-                } header: {
-                    RecentSearchesHeaderView()
-                        .textCase(.none)
                 }
+                .navigationTitle("Github")
             }
-            .navigationTitle("Github")
-        }
-        .searchable(text: $searchQueryStr,
-                    prompt: "Search Repositories")
-        .onChange(of: searchQueryStr) { newValue in
-            matchedQueries = recentlyQueries.filter { $0.hasPrefix(newValue) }
+            .searchable(text: $searchQueryStr,
+                        prompt: "Search Repositories")
+            .onSubmit(of: .search) {
+                print("search!", self.searchQueryStr)
+                self.pushActive = true
+            }
+            .onChange(of: searchQueryStr) { newValue in
+                matchedQueries = recentlyQueries.filter { $0.hasPrefix(newValue) }
+            }
+            .navigationDestination(isPresented: self.$pushActive) {
+                RepositoriesView(repoName: self.searchQueryStr)
+            }
         }
     }
 }
