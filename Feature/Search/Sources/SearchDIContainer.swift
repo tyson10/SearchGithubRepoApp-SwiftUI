34a5968cb17c84524bea090df438ac1ddfd5c343
Taeyoung Son <taeyoungson10@gmail.com>
2023-04-03 19:36:22 +0900

    feat: DIContainer 추가

---
+++ Feature/Search/Sources/SearchDIContainer.swift
@@ -0,0 +1,23 @@
+//
+//  SearchDIContainer.swift
+//  Search
+//
+//  Created by Taeyoung Son on 2023/04/03.
+//
+
+import SwiftUI
+
+import CommonUI
+
+
+public final class SearchDIContainer<ResultView: SearchResultView> {
+    private let resultViewMaker: ((String) -> ResultView)?
+    
+    public init(resultViewMaker: ((String) -> ResultView)?) {
+        self.resultViewMaker = resultViewMaker
+    }
+    
+    public func makeResultView(with query: String) -> SearchView<ResultView> {
+        return .init(resultViewMaker: resultViewMaker)
+    }
+}
