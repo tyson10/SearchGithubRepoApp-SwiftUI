f3daaa6e53f0c086c07cd42665bba5feb33619d0
Taeyoung Son <taeyoungson10@gmail.com>
2023-05-15 21:56:35 +0900

    feat: 표시할 최근 검색어 목록이 있는 경우에만 리스트 표시하도록 수정

--- Feature/Search/Sources/Presentaion/SearchView.swift
+++ Feature/Search/Sources/Presentaion/SearchView.swift
@@ -24,24 +24,26 @@ public struct SearchView<ResultView: SearchResultView>: View {
         NavigationStack {
             NavigationView {
                 List {
-                    Section {
-                        // self.recentlyQueries의 Element는 Identifiable을 준수해야 함.
-                        // 기본 상태에선 준수하지 못함.
-                        // 'id: \.self' 이 Element들의 해시값으로 구분하도록 함.
-                        // 고로, Element는 Hashable이어야 한다.
-                        ForEach(state.matchedQueries, id: \.self) { query in
-                            Button(action: {
-                                state.search(query: query)
-                            }, label: {
-                                RecentSearchesContentView(
-                                    value: query,
-                                    deleteAction: state.delete(query:)
-                                )
-                            })
+                    if !state.matchedQueries.isEmpty {
+                        Section {
+                            // self.recentlyQueries의 Element는 Identifiable을 준수해야 함.
+                            // 기본 상태에선 준수하지 못함.
+                            // 'id: \.self' 이 Element들의 해시값으로 구분하도록 함.
+                            // 고로, Element는 Hashable이어야 한다.
+                            ForEach(state.matchedQueries, id: \.self) { query in
+                                Button(action: {
+                                    state.search(query: query)
+                                }, label: {
+                                    RecentSearchesContentView(
+                                        value: query,
+                                        deleteAction: state.delete(query:)
+                                    )
+                                })
+                            }
+                        } header: {
+                            RecentSearchesHeaderView(clearAction: state.removeAllQueries)
+                                .textCase(.none)
                         }
-                    } header: {
-                        RecentSearchesHeaderView(clearAction: state.removeAllQueries)
-                            .textCase(.none)
                     }
                 }
                 .navigationTitle("Github")
