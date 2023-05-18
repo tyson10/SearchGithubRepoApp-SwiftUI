fbb7ae9362955b05077a963b5235c11b176cbe78
Taeyoung Son <taeyoungson10@gmail.com>
2023-05-18 22:31:08 +0900

    fix, refactor: URLCache에 저장된 항목 반환시 Future 사용하도록 수정 - 코드 정리

--- Core/Network/Sources/NetworkService.swift
+++ Core/Network/Sources/NetworkService.swift
@@ -10,6 +10,8 @@ import Foundation
 import Combine
 
 final public class NetworkService {
+    public typealias ResultPublisher = AnyPublisher<Data, NetworkError>
+    
     public static let baseUrl = "https://api.github.com"
     private let session: URLSession
     private let cache: URLCache
@@ -25,37 +27,43 @@ final public class NetworkService {
         session.invalidateAndCancel()
     }
     
-    public func request(endPoint: EndPoint) -> AnyPublisher<Data, NetworkError> {
+    public func request(endPoint: EndPoint) -> ResultPublisher {
         guard let request = endPoint.request else {
             return Fail(error: NetworkError.emptyRequest).eraseToAnyPublisher()
         }
         
-        // FIXME: URLCache 동작 확인
+        var resultPublisher: AnyPublisher<Data, NetworkError>
+        
         if let data = cache.cachedResponse(for: request)?.data {
-            return Just(data)
-                .mapError { error -> NetworkError in
-                    // FIXME: Warnging 해결
-                    return .unknown(error: error)
-                }
-                .eraseToAnyPublisher()
+            resultPublisher = cachedResultPublisher(with: data)
         } else {
-            return session.dataTaskPublisher(for: request)
-                .tryMap { [weak self] data, response -> Data in
-                    guard let httpResponse = response as? HTTPURLResponse,
-                          (200...299).contains(httpResponse.statusCode) else {
-                        throw NetworkError.invalidRequest
-                    }
-                    
-                    let cachedData = CachedURLResponse(response: response, data: data)
-                    
-                    self?.cache.storeCachedResponse(cachedData, for: request)
-                    
-                    return data
-                }
-                .mapError { error -> NetworkError in
-                    return .unknown(error: error)
-                }
-                .eraseToAnyPublisher()
+            resultPublisher = dataTaskPublisher(for: request)
         }
+        
+        return resultPublisher
+    }
+    
+    private func cachedResultPublisher(with data: Data) -> ResultPublisher {
+        return Future<Data, NetworkError> { $0(.success(data)) }.eraseToAnyPublisher()
+    }
+    
+    private func dataTaskPublisher(for request: URLRequest) -> ResultPublisher {
+        return session.dataTaskPublisher(for: request)
+            .tryMap { [weak self] data, response -> Data in
+                guard let httpResponse = response as? HTTPURLResponse,
+                      (200...299).contains(httpResponse.statusCode) else {
+                    throw NetworkError.invalidRequest
+                }
+                
+                let cachedData = CachedURLResponse(response: response, data: data)
+                
+                self?.cache.storeCachedResponse(cachedData, for: request)
+                
+                return data
+            }
+            .mapError { error -> NetworkError in
+                return .unknown(error: error)
+            }
+            .eraseToAnyPublisher()
     }
 }
